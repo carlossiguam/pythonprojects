e60b1a3837315da531b5e85bc9fd7212967de258
csiguaemov <carlos.sigua@gmail.com>
2020-09-20 02:36:37 -0500

    Solo cambiamos un poquito el mensaje de Flask

--- app0/app.py
+++ app0/app.py
@@ -1,9 +1,9 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hola pendejo"
+    return "hola pendejo ilustrado eso es lo que eres"
 
 if __name__ == "__main__":
     app.run()
