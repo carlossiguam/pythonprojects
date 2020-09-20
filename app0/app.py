b4ac25d661858fbdda1bc463918fd53d385d69f1
csiguaemov <carlos.sigua@gmail.com>
2020-09-20 10:04:32 -0500

    Hemos anadido una ruta para about

--- app0/app.py
+++ app0/app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def hello():
     return "hola pendejo ilustrado eso es lo que eres"
 
+@app.route("/about")
+def about():
+    return "Este sitio web es creado con el fin de palear la no automatizacion existente en la creacion de procesos dentro de la empresa."
 if __name__ == "__main__":
     app.run()
