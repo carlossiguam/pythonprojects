5f700e185697e074b54437bac79e598bb2e44dd5
carlos sigua <carlos.sigua@gmail.com>
2020-10-18 18:20:02 -0500

    archivo de pureba para creat un archivo .exe from python.

---
+++ app1/setup.py
@@ -0,0 +1,12 @@
+#setup.py
+from cx_Freeze import setup, Executable
+setup(
+    name = "boneca",
+    version = "1.0.0",
+    options = {"build_exe": {
+        'packages': ["os","sys","ctypes","win32con"],
+        'include_files': ['boneca.jpg'],
+        'include_msvcr': True,
+    }},
+    executables = [Executable("boneca.py",base="Win32GUI")]
+    )
--- appw0/src/entities/exam.py
+++ appw0/src/entities/exam.py
@@ -1,17 +1,33 @@
 # coding=utf-8
 
-from sqlalchemy import Column, String
+# from sqlalchemy import Column, String
 
-from .entity import Entity, Base
+# from .entity import Entity, Base
 
 
-class Exam(Entity, Base):
-    __tablename__ = 'exams'
+# class Exam(Entity, Base):
+#     __tablename__ = 'exams'
 
-    title = Column(String)
-    description = Column(String)
+#     title = Column(String)
+#     description = Column(String)
 
-    def __init__(self, title, description, created_by):
-        Entity.__init__(self, created_by)
-        self.title = title
-        self.description = description
+#     def __init__(self, title, description, created_by):
+#         Entity.__init__(self, created_by)
+#         self.title = title
+#         self.description = description
+
+# coding=utf-8
+
+from marshmallow import Schema, fields
+
+# ... other import statements ...
+
+# ... Exam class definition ...
+
+class ExamSchema(Schema):
+    id = fields.Number()
+    title = fields.Str()
+    description = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
